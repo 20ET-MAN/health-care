9520739c56d7b82669b4fbe541d070f4b5d30736
namngotien <tiennam69ht22@gmail.com>
2022-04-16 15:20:46 +0700

    add basic logic and fix err home page

--- lib/src/presentation/screens/home/home_screen.dart
+++ lib/src/presentation/screens/home/home_screen.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:healthcare/src/presentation/config/app_color.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key, this.isShow = true}) : super(key: key);
+  final bool isShow;
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final bool isShow = true;
+
+  int _selectedIndex = 0;
+
+  void _onTapItem(int index) {
+    _selectedIndex = index;
+    setState(() {});
+  }
+
+  final List<Widget> _items = [
+    const Text(
+      'Index 0: Home',
+    ),
+    const Text(
+      'Index 1: Profile',
+    ),
+    const Text(
+      'Index 3: Shop',
+    ),
+    const Text(
+      'Index 4: Shop',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColor.colorGrey,
       appBar: AppBar(
         actions: [
           Container(
@@ -13,11 +44,47 @@ class HomeScreen extends StatelessWidget {
                 border: Border.all(
                     color: Colors.black, width: 1, style: BorderStyle.solid),
               ),
-              child: Icon(Icons.ac_unit)),
+              child: const Icon(Icons.ac_unit)),
         ],
-        leading: Icon(Icons.print),
       ),
-      body: Column(),
+      body: Center(
+        child: _items.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: _showBottomNav(),
+    );
+  }
+
+  Widget _showBottomNav() {
+    return BottomNavigationBar(
+      backgroundColor: AppColor.colorWhile,
+      selectedItemColor: AppColor.colorOrange,
+      currentIndex: _selectedIndex,
+      unselectedItemColor: Colors.grey,
+      onTap: _onTapItem,
+      elevation: 2,
+      items: [
+        isShow
+            ? const BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              )
+            : const BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                label: 'Profile',
+              ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.shopping_bag),
+          label: 'Shop',
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.ac_unit_outlined),
+          label: 'Unit',
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.accessibility_new),
+          label: 'Man',
+        ),
+      ],
     );
   }
 }
